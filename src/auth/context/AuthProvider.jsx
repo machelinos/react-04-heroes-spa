aa6950535b2561d0929df7b7f35525aa2f3462a6
Marcel Cabrera <marcel.cabrera@gmail.com>
2022-10-03 00:03:15 -0500

    Heroes App - Auth - User active persistance

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -6,15 +6,26 @@ const initialState = {
     logged: false
 }
 
+const init = () => {
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    return {
+        logged: !!user,
+        user
+    }
+}
+
 export const AuthProvider = ({children}) => {
 
-    const [authState, dispatch] = useReducer(authReducer, initialState);
+    const [authState, dispatch] = useReducer(authReducer, initialState, init);
 
     const login = (user) =>{
         const loginAction = {
             type: types.login,
             payload: user
         }
+
+        localStorage.setItem('user', JSON.stringify(user));
 
         dispatch(loginAction);
     }
